27b2fd1fcaa591add1c10873c0983346e7aee1d8
Aaron <aaronrebmann@gmail.com>
2025-08-07 09:32:59 +0200

    add vscode install to client setup

--- client-setup.sh
+++ client-setup.sh
@@ -15,3 +15,20 @@ source $HOME/.local/bin/env
 
 
 uv add ansible
+
+
+## vscode
+
+
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+rm -f microsoft.gpg
+
+
+sudo echo 'Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg' > /etc/apt/sources.list.d/vscode.sources
