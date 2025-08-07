072039aa6c31eb34c049d57aa45f074772b02939
Aaron <aaronrebmann@gmail.com>
2025-08-07 08:54:14 +0200

    feat: Add minimal requirements

---
+++ client-setup.sh
@@ -0,0 +1,17 @@
+!#/bin/bash
+
+
+# PACKAGES
+
+sudo apt install keepassxc
+
+sudo apt install xclip
+
+sudo apt install nextcloud-client
+
+## uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+
+
+uv add ansible
